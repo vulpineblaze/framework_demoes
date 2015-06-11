0d3afc2210034ca76cf6140f74889ad53e92b126
vulpineblaze <vulpineblazeyt@gmail.com>
2015-06-10 23:20:16 -0700

    finished tut page: shooting-2

--- pixelnest_tut/Assets/scripts/PlayerScript.cs
+++ pixelnest_tut/Assets/scripts/PlayerScript.cs
@@ -23,6 +23,21 @@ public class PlayerScript : MonoBehaviour
 		movement = new Vector2(
 			speed.x * inputX,
 			speed.y * inputY);
+
+		// 5 - Shooting
+		bool shoot = Input.GetButtonDown("Fire1");
+		shoot |= Input.GetButtonDown("Fire2");
+		// Careful: For Mac users, ctrl + arrow is a bad idea
+		
+		if (shoot)
+		{
+			WeaponScript weapon = GetComponent<WeaponScript>();
+			if (weapon != null)
+			{
+				// false because the player is not an enemy
+				weapon.Attack(false);
+			}
+		}
 		
 	}
 	
