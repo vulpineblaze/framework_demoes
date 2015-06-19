f294667d840dfe06c8dd276de0bb4c9e50a69996
labtech <vulpineblazeyt@gmail.com>
2015-06-18 22:14:49 -0700

    upgraded to 5.1.1

---
+++ pixelnest_tut_5-1-1/Assets/scripts/EnemyScript.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Enemy generic behavior
+/// </summary>
+public class EnemyScript : MonoBehaviour
+{
+	private bool hasSpawn;
+	private MoveScript moveScript;
+	private WeaponScript[] weapons;
+	
+	void Awake()
+	{
+		// Retrieve the weapon only once
+		weapons = GetComponentsInChildren<WeaponScript>();
+		
+		// Retrieve scripts to disable when not spawn
+		moveScript = GetComponent<MoveScript>();
+	}
+	
+	// 1 - Disable everything
+	void Start()
+	{
+		hasSpawn = false;
+		
+		// Disable everything
+		// -- collider
+		GetComponent<Collider2D>().enabled = false;
+		// -- Moving
+		moveScript.enabled = false;
+		// -- Shooting
+		foreach (WeaponScript weapon in weapons)
+		{
+			weapon.enabled = false;
+		}
+	}
+	
+	void Update()
+	{
+		// 2 - Check if the enemy has spawned.
+		if (hasSpawn == false)
+		{
+			if (GetComponent<Renderer>().IsVisibleFrom(Camera.main))
+			{
+				Spawn();
+			}
+		}
+		else
+		{
+			// Auto-fire
+			foreach (WeaponScript weapon in weapons)
+			{
+				if (weapon != null && weapon.enabled && weapon.CanAttack)
+				{
+					weapon.Attack(true);
+				}
+			}
+			
+			// 4 - Out of the camera ? Destroy the game object.
+			if (GetComponent<Renderer>().IsVisibleFrom(Camera.main) == false)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+	
+	// 3 - Activate itself.
+	private void Spawn()
+	{
+		hasSpawn = true;
+		
+		// Enable everything
+		// -- Collider
+		GetComponent<Collider2D>().enabled = true;
+		// -- Moving
+		moveScript.enabled = true;
+		// -- Shooting
+		foreach (WeaponScript weapon in weapons)
+		{
+			weapon.enabled = true;
+		}
+	}
+}
---
+++ pixelnest_tut_5-1-1/Assets/scripts/PlayerScript.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Player controller and behavior
+/// </summary>
+public class PlayerScript : MonoBehaviour
+{
+	/// <summary>
+	/// 1 - The speed of the ship
+	/// </summary>
+	public Vector2 speed = new Vector2(50, 50);
+	
+	// 2 - Store the movement
+	private Vector2 movement;
+	
+	void Update()
+	{
+		// 3 - Retrieve axis information
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		// 4 - Movement per direction
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+
+		// 5 - Shooting
+		bool shoot = Input.GetButtonDown("Fire1");
+		shoot |= Input.GetButtonDown("Fire2");
+		// Careful: For Mac users, ctrl + arrow is a bad idea
+		
+		if (shoot)
+		{
+			WeaponScript weapon = GetComponent<WeaponScript>();
+			if (weapon != null)
+			{
+				// false because the player is not an enemy
+				weapon.Attack(false);
+			}
+		}
+
+		// 6 - Make sure we are not outside the camera bounds
+		var dist = (transform.position - Camera.main.transform.position).z;
+		
+		var leftBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 0, dist)
+			).x;
+		
+		var rightBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(1, 0, dist)
+			).x;
+		
+		var topBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 0, dist)
+			).y;
+		
+		var bottomBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 1, dist)
+			).y;
+		
+		transform.position = new Vector3(
+			Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
+			Mathf.Clamp(transform.position.y, topBorder, bottomBorder),
+			transform.position.z
+			);
+		
+	}
+	
+	void FixedUpdate()
+	{
+		// 5 - Move the game object
+		GetComponent<Rigidbody2D>().velocity = movement;
+	}
+}
