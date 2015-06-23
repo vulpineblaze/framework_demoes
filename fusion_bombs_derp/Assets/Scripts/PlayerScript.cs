1cd837d3f78ebd2ddae426c0f9685ef61a8937e5
jsmunster <jeffreymunster@gmail.com>
2015-06-23 14:15:41 -0700

    Player on screen and commically fast movement

---
+++ fusion_bombs_derp/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Player controller and behavior
+/// </summary>
+public class PlayerScript : MonoBehaviour
+{
+  /// <summary>
+  /// 1 - The speed of the ship
+  /// </summary>
+  public Vector2 speed = new Vector2(50, 50);
+
+  // 2 - Store the movement
+  private Vector2 movement;
+
+  void Update()
+  {
+    // 3 - Retrieve axis information
+    float inputX = Input.GetAxis("Horizontal");
+    float inputY = Input.GetAxis("Vertical");
+
+    // 4 - Movement per direction
+    movement = new Vector2(
+      speed.x * inputX,
+      speed.y * inputY);
+
+  }
+
+  void FixedUpdate()
+  {
+    // 5 - Move the game object
+    GetComponent<Rigidbody2D>().velocity = movement;
+  }
+}
