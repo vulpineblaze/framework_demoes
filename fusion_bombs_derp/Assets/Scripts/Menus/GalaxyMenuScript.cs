2c0dcb41ea642a2e115af856aa4edd9f9602eb98
labtech <vulpineblazeyt@gmail.com>
2015-07-28 20:11:39 -0700

    got a list-menu onscreen that can change scenes; press 'm'

--- fusion_bombs_derp/Assets/Scripts/Menus/GalaxyMenuScript.cs
+++ fusion_bombs_derp/Assets/Scripts/Menus/GalaxyMenuScript.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class GalaxyMenuScript : MonoBehaviour {
 
-	String availableLevels[];
+	// String availableLevels[];
+	string[] availableLevels = {"Menu", "Stage1", "Stage2"};
 	int windowWidth = 200;
 	int windowHeight = 200;
-	private boolean showMenu = false;
+	private bool showMenu = false;
 	
 	// Use this for initialization
 	void Start () {
@@ -15,8 +16,41 @@ public class GalaxyMenuScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetButtonDown("MapToggle")){
+			showMenu = !showMenu;
+		}
 	}
+
+	void OnGUI(){
+
+		if (showMenu) { 
+			GUI.Window(0, 
+					new Rect( (Screen.width / 2) - (windowWidth / 2), 
+						(Screen.height / 2) - (windowHeight / 2), 
+						windowWidth, 
+						windowHeight), 
+					LevelSelect, 
+					"Select a level!"); 
+		}
+//Creates a window with ID 0 in the center of the screen using the function LevelSelect with the title, "Select a level!" } }
+
+	}
+
+	void LevelSelect (int id) { 
+		foreach (string levelName in availableLevels) { 
+			if (GUILayout.Button(levelName)) { 
+				showMenu = false; 
+				Application.LoadLevel(levelName); 
+
+			} 
+		} 
+		if (GUILayout.Button("Cancel")) { 
+		//Gives the player the opportunity to back out of the menu. 
+			showMenu = false; 
+		} 
+	}
+
+
 }
 
 /*
