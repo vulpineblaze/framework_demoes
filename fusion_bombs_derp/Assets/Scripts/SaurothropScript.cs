e928b17968a05543c0cb51d3ecfe27ccbe141a1e
vulpineblazeyt <vulpineblazeyt@gmail.com>
2015-08-14 10:03:59 -0700

    assembled ST ship sprites, move isnt working right

---
+++ fusion_bombs_derp/Assets/Scripts/SaurothropScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaurothropScript : MonoBehaviour {
+
+	private GameObject player;
+	private WeaponScript[] weapons;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Hand");	
+	}
+	
+	void Awake()
+	{
+	// Retrieve the weapon only once
+		weapons = GetComponentsInChildren<WeaponScript>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float newDistance = 0;
+		float bestDistance = 0;
+		WeaponScript closest = null;
+		foreach (WeaponScript weapon in weapons)
+	    {
+	      // Auto-fire
+	    	newDistance = Vector3.Distance(player.transform.position, weapon.transform.position);
+			if(newDistance < bestDistance){
+				bestDistance = newDistance;
+				closest = weapon;
+			}			
+	    }
+
+	    //if weapon is closest then fire
+    	if (closest != null && closest.CanAttack)
+		{
+			closest.Attack(true);
+			SoundEffectsHelper.Instance.MakeEnemyShotSound();
+		}
+
+	}
+}
