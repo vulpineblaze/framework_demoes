068c62d1477fcd87c3e99d81bb70d12c963f2151
labtech <vulpineblazeyt@gmail.com>
2015-08-14 21:06:18 -0700

    ST ship is working pretty good now. gotta doublecheck issue.

--- fusion_bombs_derp/Assets/Scripts/SaurothropScript.cs
+++ fusion_bombs_derp/Assets/Scripts/SaurothropScript.cs
@@ -5,10 +5,14 @@ public class SaurothropScript : MonoBehaviour {
 
 	private GameObject player;
 	private WeaponScript[] weapons;
+	private WeaponScript closest;
+	private float newDistance;
+	private float bestDistance = 999999999999; // must be high
+	private float[] testArray = new float[5];
 
 	// Use this for initialization
 	void Start () {
-        player = GameObject.Find("Hand");	
+        player = GameObject.Find("Player");	
 	}
 	
 	void Awake()
@@ -19,13 +23,17 @@ public class SaurothropScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		float newDistance = 0;
-		float bestDistance = 0;
-		WeaponScript closest = null;
+		newDistance = 0;
+		bestDistance = 99999999999;
+		int i = 0;
 		foreach (WeaponScript weapon in weapons)
 	    {
 	      // Auto-fire
+
 	    	newDistance = Vector3.Distance(player.transform.position, weapon.transform.position);
+			
+			testArray[i] = newDistance;
+			i++;
 			if(newDistance < bestDistance){
 				bestDistance = newDistance;
 				closest = weapon;
